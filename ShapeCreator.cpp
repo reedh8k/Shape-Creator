50b940899a8fc0e0928961f63f38e7d4534a7c61
reedh8k <reedh8k@gmail.com>
2025-12-18 10:48:16 -0500

    added more correct input requirements

--- ShapeCreator.cpp
+++ ShapeCreator.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 #include <fstream>
+
 #include <iomanip>
 
 using namespace std;
@@ -8,104 +10,158 @@ using namespace std;
 //Square: length and width based on rows & columns of the character
 //Triangle: sides based on total of a side characters (including blank spaces between columns)
 //Diamond: sides based on total of a side characters (including blank spaces between columns)
-int main(){
-    ofstream outFile("C:\\Users\\99210\\Documents\\GitHub\\Shape-Creator\\ShapeCreator.txt");
-    if (outFile.is_open()){
-        bool cont = true;
-        while (cont == true){
-            char character;
-            int size;
-            char shape;
-            //asks for desired shape
-            cout << "Please input the shape you want out of the following: s = square, t = triangle, d = diamond, o = other.\n";
-            cin >> shape;
-            //identifies shape
-            if (shape == 's'){ //square
-                cout << "You have selected: Square\nPlease input the character used to make up the square and the size.\n";
-                cin >> character;
-                cin >> size;
-                if (size > 0){
-                    for (int j = 1; j <= size; j++){
-                        for (int i = 1; i <= size; i++){
-                            outFile << character << setw(3);
+int main() {
+    while ((2 + 2) == 4) {
+        char sel;
+        cout << "Please input whether you would like to use the shape creator or play the game (s/g)\n";
+        cin >> sel;
+        if (sel == 's') {
+            int shapeCount = 0;
+            ofstream outFile("C:\\Users\\99210\\Documents\\GitHub\\Shape-Creator\\ShapeCreator.txt");
+            if (outFile.is_open()) {
+                bool cont = true;
+                while (cont == true) {
+                    char character;
+                    int size;
+                    char shape;
+                    //asks for desired shape
+                    cout << "Please input the shape you want out of the following: s = square, t = triangle, d = diamond.\n";
+                    cin >> shape;
+                    //identifies shape
+                    if (shape == 's') { //square
+                        cout << "You have selected: Square\nPlease input the character used to make up the square and the size.\n";
+                        cin >> character;
+                        cin >> size;
+                        if (size > 0) {
+                            for (int j = 1; j <= size; j++) {
+                                for (int i = 1; i <= size; i++) {
+                                    outFile << character << setw(3);
+                                }
+                                outFile << "\n";
+                            }
+                            shapeCount++;
+                        } else cout << "Invalid input for size.";
+                    } else if (shape == 't') { //triangle
+                        char option;
+                        //specifies triangle type
+                        cout << "You have selected: Triangle\nPlease input one of the following: e = equilateral, r = right\n";
+                        cin >> option;
+                        //identifies type
+                        if (option == 'e') { //equilateral
+                            cout << "Please select the character that will make up the shape, and the size of a side, separated by spaces.\n";
+                            cin >> character;
+                            cin >> size;
+                            if (size > 0) {
+                                outFile << setw(1 + size * .5) << character << endl;
+                                for (int i = 1; i <= (size * .433); i++) { //.433 is the rough estimate of amount of rows based on height and space between rows (.866/2)
+                                    outFile << setw(1 + size * .5 - i) << character;
+                                    outFile << setw(2 * i) << character << endl;
+                                }
+                                for (int i = 1; i <= size; i++) {
+                                    outFile << character;
+                                }
+                                outFile << endl;
+                                shapeCount++;
+                            } else {
+                                cout << "Invalid size input.\n";
+                            }
+                        } else if (option == 'r') {
+                            int height;
+                            cout << "Please select the character that will make up the shape, and the length of the base, and the height, separated by spaces.\n";
+                            cin >> character;
+                            cin >> size;
+                            cin >> height;
+                            if (size > 0 && height > 0) {
+                                outFile << character << endl;
+                                for (int i = 1; i <= (height / 2.5); i++) { //rough estimate to help with space between rows(not applied in square creator)
+                                    outFile << character;
+                                    outFile << setw(i * 1.5) << character << endl;
+                                }
+                                for (int i = 1; i <= size; i++) {
+                                    outFile << character;
+                                }
+                                outFile << endl;
+                                shapeCount++;
+                            } else {
+                                cout << "Invalid input for size.\n";
+                            }
+                        } else {
+                            cout << "Invalid input.";
                         }
-                        outFile << "\n";
-                    }
-                } else cout << "Invalid input for size.";
-            } else if (shape == 't'){ //triangle
-                char option;
-                //specifies triangle type
-                cout << "You have selected: Triangle\nPlease input one of the following: e = equilateral, r = right\n";
-                cin >> option;
-                //identifies type
-                if (option == 'e'){ //equilateral
-                    cout << "Please select the character that will make up the shape, and the size of a side, separated by spaces.\n";
-                    cin >> character;
-                    cin >> size;
-                    outFile << setw(1+size*.5) << character << endl;
-                    for (int i = 1; i <= (size*.433); i++){ //.433 is the rough estimate of amount of rows based on height and space between rows (.866/2)
-                        outFile << setw(1+size*.5-i) << character;
-                        outFile << setw(2*i) << character << endl;
-                    }
-                    for (int i = 1; i <= size; i++){
-                        outFile << character;
-                    }
-                    outFile << endl;
-                } else if (option == 'r'){
-                    int height;
-                    cout << "Please select the character that will make up the shape, and the length of the base, and the height, separated by spaces.\n";
-                    cin >> character;
-                    cin >> size;
-                    cin >> height;
-                    outFile << character << endl;
-                    for (int i = 1; i <= (height/2.5); i++){ //rough estimate to help with space between rows(not applied in square creator)
-                        outFile << character;
-                        outFile << setw(i*1.5) << character << endl;
-                    }
-                    for (int i = 1; i <= size; i++){
-                        outFile << character;
+                    } else if (shape == 'd') {
+                        cout << "Please select the character that will make up the shape, and the size of a side, separated by spaces. (Diamond works best with larger numbers)\n";
+                        cin >> character;
+                        cin >> size;
+                        if (size > 0) {
+                            outFile << setw(1 + size * .5) << character << endl;
+                            for (int i = 1; i <= (size * .433); i++) { //.433 is the rough estimate of amount of rows based on height and space between rows (.866/2)
+                                outFile << setw(1 + size * .5 - i) << character;
+                                outFile << setw(2 * i) << character << endl;
+                            }
+                            for (int i = (size * .43); i >= 1; i--) {
+                                outFile << setw(1 + size * .5 - i) << character;
+                                outFile << setw(2 * i) << character << endl;
+                            }
+                            outFile << setw(1 + size * .5) << character << endl;
+                            outFile << endl;
+                            shapeCount++;
+                        } else {
+                            cout << "Invalid input for size.\n";
+                        }
+                    } else {
+                        cout << "Invalid input.\n";
                     }
                     outFile << endl;
-                } else {
-                    cout << "Invalid input.";
-                } 
-            } else if (shape == 'd'){
-                cout << "Please select the character that will make up the shape, and the size of a side, separated by spaces. (Diamond works best with larger numbers)\n";
-                    cin >> character;
-                    cin >> size;
-                    outFile << setw(1+size*.5) << character << endl;
-                    for (int i = 1; i <= (size*.433); i++){ //.433 is the rough estimate of amount of rows based on height and space between rows (.866/2)
-                        outFile << setw(1+size*.5-i) << character;
-                        outFile << setw(2*i) << character << endl;
-                    }
-                    for (int i = (size*.43); i >= 1; i--){
-                        outFile << setw(1+size*.5-i) << character;
-                        outFile << setw(2*i) << character << endl;
-                    }
-                    outFile << setw(1+size*.5) << character << endl;
-                    outFile << endl;
-            }
-            outFile << endl;
-            char another;
-            cout << "Would you like to print another shape? (y/n)\n";
-            cin >> another;
-            if (another == 'n'){
-                cont = false;
-            }
-        }
-    } else cout << "File is not open.";
-    outFile.close();
+                    char another;
 
-    //Confirms file output
-    string line;
-    ifstream inFile("C:\\Users\\99210\\Documents\\GitHub\\Shape-Creator\\ShapeCreator.txt");
-    cout<<"\nReading from \"C:\\Users\\99210\\Documents\\GitHub\\Shape-Creator\\ShapeCreator.txt\"\nYou have created the following shapes:" << endl;
-    if(inFile.is_open()){
-        while(getline(inFile, line)){ //reads line by line
-            cout << line << endl;
+                    if (shapeCount == 1) {
+                        cout << "You have created " << shapeCount << " shape.\n";
+                    } else {
+                        cout << "You have created " << shapeCount << " shapes.\n";
+                    }
+                    bool next = false;
+                    while (next == false) {
+                        cout << "Would you like to create another shape? (y = yes, n = no, e = exit)\n";
+                        cin >> another;
+                        if (another == 'n') {
+                            cont = false;
+                            next = true;
+                        } else if (another == 'y') {
+                            next = true;
+                        } else if (another == 'e') {
+                            return 0;
+                        } else {
+                            cout << "Invalid input.\n";
+                        }
+                    }
+                }
+            } else cout << "File is not open for output.";
+            outFile.close();
+
+            //Confirms file output
+            string line;
+            ifstream inFile("C:\\Users\\99210\\Documents\\GitHub\\Shape-Creator\\ShapeCreator.txt");
+
+            cout << "\nReading from \"C:\\Users\\99210\\Documents\\GitHub\\Shape-Creator\\ShapeCreator.txt\"" << endl;
+            if (shapeCount == 0) {
+                cout << "You have created 0 shapes\n";
+            } else if (shapeCount == 1) {
+                cout << "You have created the following 1 shape: \n";
+            } else {
+                cout << "You have created the following " << shapeCount << " shapes: " << endl;
+            }
+            if (inFile.is_open()) {
+                while (getline(inFile, line)) { //reads line by line
+                    cout << line << endl;
+                }
+                inFile.close();
+            } else {
+                cout << "Unable to open file for reading" << endl;
+            }
+        } else if (sel == 'g') {
+            cout << "hi\n";
+        } else {
+            cout << "Invalid input.\n";
         }
-        inFile.close();
-    } else{
-        cout << "Unable to open file for reading" << endl;
     }
 }
